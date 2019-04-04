f3f61783896c4b324c01fc2532012bcf425036c1
Ankit Prakash <43018227+ankitprakash687@users.noreply.github.com>
2019-04-03 22:12:20 -0500

    S533987 - In home activity change the functionality to add and hide different fragments instead of replacing it.

--- app/src/main/java/com/example/swapu/home/HomeActivity.java
+++ app/src/main/java/com/example/swapu/home/HomeActivity.java
@@ -3,19 +3,55 @@ package com.example.swapu.home;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import com.example.swapu.home.account.AccountFragment;
 import com.example.swapu.home.message.MessageFragment;
 import com.example.swapu.R;
 import com.example.swapu.home.sell.SellFragment;
+import com.example.swapu.home.account.AccountFragment;
 import com.parse.ParseUser;
 
 public class HomeActivity extends AppCompatActivity {
-public static String data;
-   // TextView tvName, tvEmail;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
+    private static final int CAMERA_REQUEST = 4;
+    public static String data;
+    // TextView tvName, tvEmail;
+    Fragment homeFragment;
+    Fragment messageFragment;
+    Fragment sellFragment;
+    Fragment accountFragment;
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    //   Fragment selectedFragment = null;
+                    switch (menuItem.getItemId()) {
+                        case R.id.navigation_home:
+                            //  selectedFragment = new HomeFragment();
+                            displayHomeFragment();
+                            break;
+                        case R.id.navigation_sell:
+                            //   selectedFragment = new SellFragment();
+                            displaySellFragment();
+                            break;
+                        case R.id.navigation_messages:
+                            //   selectedFragment = new MessageFragment();
+                            displayMessageFragment();
+                            break;
+                        case R.id.navigation_account:
+                            //     selectedFragment = new AccountFragment();
+                            displayAccountFragment();
+                            break;
+                    }
+
+                    //  getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+                }
+            };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,56 +61,107 @@ public static String data;
 //        TextView sample = findViewById(R.id.sample_textview);
 //        sample.setText(data);
         ParseUser currentUser = ParseUser.getCurrentUser();
-        Fragment selectedFragment = new HomeFragment();
 
+        //  Fragment selectedFragment = new HomeFragment();
         if (savedInstanceState == null){
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();}
-//        tvName = findViewById(R.id.tvName);
-//        tvEmail = findViewById(R.id.tvEmail);
+            //  getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            homeFragment = new HomeFragment();
+            messageFragment = new MessageFragment();
+            sellFragment = new SellFragment();
+            accountFragment = new AccountFragment();
+            displayHomeFragment();
 
+        }
         bottomNav.setOnNavigationItemSelectedListener(navListener);
-
-//        if(currentUser!=null){
-//            tvName.setText(currentUser.getString("name"));
-//            tvEmail.setText(currentUser.getEmail());
-//        }
     }
 
-//    public void logout(View view) {
-//        ProgressDialog progress = new ProgressDialog(this);
-//        progress.setMessage("Loading ...");
-//        progress.show();
-//        ParseUser.logOut();
-//        Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
-//        startActivity(intent);
-//        finish();
-//        progress.dismiss();
-//    }
+    private void displayHomeFragment() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        if (homeFragment.isAdded()) { // if the fragment is already in container
+            ft.show(homeFragment);
+        } else { // fragment needs to be added to frame container
+            ft.add(R.id.fragment_container, homeFragment);
+        }
+        // Hide fragment B
+        if (messageFragment.isAdded()) {
+            ft.hide(messageFragment);
+        }
+        // Hide fragment C
+        if (sellFragment.isAdded()) {
+            ft.hide(sellFragment);
+        }
+        if (accountFragment.isAdded()) {
+            ft.hide(accountFragment);
+        }
+        // Commit changes
+        ft.commit();
+    }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener(){
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                    Fragment selectedFragment = null;
-                    switch (menuItem.getItemId()){
-                        case R.id.navigation_home:
-                            selectedFragment = new HomeFragment();
-                            break;
-                        case R.id.navigation_sell:
-                            selectedFragment = new SellFragment();
-                            break;
-                        case R.id.navigation_messages:
-                            selectedFragment = new MessageFragment();
-                            break;
-                        case R.id.navigation_account:
-                            selectedFragment = new AccountFragment();
-                            break;
-                    }
+    private void displayMessageFragment() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        if (messageFragment.isAdded()) { // if the fragment is already in container
+            ft.show(messageFragment);
+        } else { // fragment needs to be added to frame container
+            ft.add(R.id.fragment_container, messageFragment);
+        }
+        // Hide fragment B
+        if (homeFragment.isAdded()) {
+            ft.hide(homeFragment);
+        }
+        // Hide fragment C
+        if (sellFragment.isAdded()) {
+            ft.hide(sellFragment);
+        }
+        if (accountFragment.isAdded()) {
+            ft.hide(accountFragment);
+        }
+        // Commit changes
+        ft.commit();
+    }
 
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
-                    return true;
-                }
-            };
+    private void displaySellFragment() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        if (sellFragment.isAdded()) { // if the fragment is already in container
+            ft.show(sellFragment);
+        } else { // fragment needs to be added to frame container
+            ft.add(R.id.fragment_container, sellFragment);
+        }
+        // Hide fragment B
+        if (messageFragment.isAdded()) {
+            ft.hide(messageFragment);
+        }
+        // Hide fragment C
+        if (accountFragment.isAdded()) {
+            ft.hide(accountFragment);
+        }
+        if (homeFragment.isAdded()) {
+            ft.hide(homeFragment);
+        }
+        // Commit changes
+        ft.commit();
+    }
+
+    private void displayAccountFragment() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        if (accountFragment.isAdded()) { // if the fragment is already in container
+            ft.show(accountFragment);
+        } else { // fragment needs to be added to frame container
+            ft.add(R.id.fragment_container, accountFragment);
+        }
+        // Hide fragment B
+        if (messageFragment.isAdded()) {
+            ft.hide(messageFragment);
+        }
+        // Hide fragment C
+        if (sellFragment.isAdded()) {
+            ft.hide(sellFragment);
+        }
+        if (homeFragment.isAdded()) {
+            ft.hide(homeFragment);
+        }
+        // Commit changes
+        ft.commit();
+    }
 
 
 }
