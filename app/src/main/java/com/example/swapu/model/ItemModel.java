2fd04b9935cc72df1f718c1ba18854d76facf61c
Ankit Prakash <43018227+ankitprakash687@users.noreply.github.com>
2019-04-03 22:48:04 -0500

    S533987 - updated home fragment layout and Item Model class

--- app/src/main/java/com/example/swapu/model/ItemModel.java
+++ app/src/main/java/com/example/swapu/model/ItemModel.java
@@ -2,20 +2,33 @@ package com.example.swapu.model;
 
 import android.graphics.Bitmap;
 
-
+import java.io.Serializable;
 import java.util.Date;
 
-public class ItemModel {
+public class ItemModel implements Serializable {
 
 String title;
 Bitmap image;
 Date postDate;
+    String price;
 
-public ItemModel(String title, Date postDate, Bitmap image){
+    String objectId;
+
+    public ItemModel(String title, Date postDate, Bitmap image, String objectId, String price) {
     this.title = title;
     this.image = image;
     this.postDate = postDate;
-}
+        this.objectId = objectId;
+        this.price = price;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
 
     public String getTitle() {
         return title;
@@ -41,8 +54,12 @@ public ItemModel(String title, Date postDate, Bitmap image){
         this.postDate = postDate;
     }
 
-    @Override
-    public String toString() {
-        return title;
+    public String getObjectId() {
+        return objectId;
     }
+
+    public void setObjectId(String objectId) {
+        this.objectId = objectId;
+    }
+
 }
