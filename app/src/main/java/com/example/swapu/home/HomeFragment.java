047cfd0765965cbf2031e153071f2dd06a0fade4
Ankit Prakash <43018227+AnkitPrakash687@users.noreply.github.com>
2019-04-03 22:49:48 -0500

    S533987 - updated home fragment: check internet before doing network operation

--- app/src/main/java/com/example/swapu/home/HomeFragment.java
+++ app/src/main/java/com/example/swapu/home/HomeFragment.java
@@ -14,24 +14,25 @@ import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
 import com.example.swapu.App;
-import com.example.swapu.model.ItemModel;
-import com.example.swapu.home.map.MapsActivity;
 import com.example.swapu.R;
+import com.example.swapu.common.ComUtils;
+import com.example.swapu.home.map.MapsActivity;
+import com.example.swapu.home.product.ProductActivity;
+import com.example.swapu.model.ItemModel;
 import com.parse.FindCallback;
 import com.parse.GetDataCallback;
 import com.parse.ParseException;
@@ -40,13 +41,15 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements GetNearByLocation.GetNearByAsyncTaskCallback {
     private View mContentView = null;
     protected LocationManager locationManager;
     protected LocationListener locationListener;
@@ -59,26 +62,23 @@ public class HomeFragment extends Fragment {
     protected String latitude, longitude;
     protected boolean gps_enabled, network_enabled;
     public ArrayList<ItemModel> dataList = new ArrayList();
-
+    final static String KEY = "HOME_CACHE";
+    ItemAdapter itemAdapter;
+    ListView itemGridview;
+    Button location;
+    List<Integer> nearByZipCodeList;
+    ArrayList<ItemModel> itemModels;
+    String city;
+    SharedPreferences sharedPreferences;
+    String searchText;
+    Button searchButton;
+    private Toolbar toolbar;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mContentView = inflater.inflate(R.layout.fragment_home, container, false);
         context = container.getContext();
-
         final App globalVariable = (App) context.getApplicationContext();
-        GridView itemGridview = mContentView.findViewById(R.id.item_gridview);
-        Button location = mContentView.findViewById(R.id.location_button);
-       // getNearByLocation();
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-        String zipCode = sharedPreferences.getString("zipCode","");
-        String city = sharedPreferences.getString("location","");
-        location.setText(city.split(",")[0]);
-        findObjects();
-        ItemAdapter itemAdapter = new ItemAdapter(context, R.layout.gridview_items, dataList);
-        itemGridview.setAdapter(itemAdapter);
-
-
 
         return mContentView;
     }
@@ -87,11 +87,20 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         Button location = mContentView.findViewById(R.id.location_button);
-        GridView itemGridview = mContentView.findViewById(R.id.item_gridview);
+
+        // Setting toolbar as the ActionBar with setSupportActionBar() call
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        final String zipCode = sharedPreferences.getString("zipCode", "");
+        city = sharedPreferences.getString("location", "");
+        itemGridview = mContentView.findViewById(R.id.item_gridview);
+        location = mContentView.findViewById(R.id.location_button);
+        location.setText(city.split(",")[0]);
+
+        itemAdapter = new ItemAdapter(context, R.layout.gridview_items, dataList);
+        getNearByLocation();
       //  ParseQuery<ParseObject> query = ParseQuery.getQuery("Product");
        // Query Parameters
-
-
         location.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -99,6 +108,29 @@ public class HomeFragment extends Fragment {
                 startActivityForResult(mapIntent, 1);
             }
 
+        });
+
+        searchButton = mContentView.findViewById(R.id.search_button);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mapIntent = new Intent(getActivity(), SearchActivity.class);
+                startActivityForResult(mapIntent, 6);
+            }
+        });
+
+        // Set an item click listener for GridView widget
+        itemGridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView title = view.findViewById(R.id.title_textview);
+                String objectId = title.getTag().toString();
+                Intent productIntent = new Intent(getContext(), ProductActivity.class);
+                productIntent.putExtra("objectId", objectId);
+                productIntent.putExtra("title", title.getText().toString());
+                productIntent.putExtra("zipCode", zipCode);
+                startActivity(productIntent);
+            }
         });
 
 
@@ -110,11 +142,16 @@ public class HomeFragment extends Fragment {
         if (requestCode == 1) {
             if (resultCode == Activity.RESULT_OK) {
                 locationDetails = data.getStringExtra("location");
-                if(dataList.size()>0){
-                    dataList.clear();
-                }
-                findObjects();
-
+                refresh();
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                //Write your code if there's no result
+            }
+        } else if (requestCode == 6) {
+            if (resultCode == Activity.RESULT_OK) {
+                searchText = data.getStringExtra("searchText");
+                searchButton.setText(searchText);
+                refresh();
             }
             if (resultCode == Activity.RESULT_CANCELED) {
                 //Write your code if there's no result
@@ -122,75 +159,108 @@ public class HomeFragment extends Fragment {
         }
     }
 
-    @Override
-    public void onResume() {
 
-        super.onResume();
-        if (!getUserVisibleHint()) {
-            return;
+    public void refresh() {
+        if (dataList.size() > 0) {
+            dataList.clear();
+            itemAdapter.notifyDataSetChanged();
         }
-        Button location = mContentView.findViewById(R.id.location_button);
-        if (locationDetails != null) {
-            String place[] = locationDetails.split(",");
-//            final App globalVariable = (App) context.getApplicationContext();
-//            globalVariable.setLocation(place[0] + ", " + place[1]);
-//            globalVariable.setPincode(place[2]);
-            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-            SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.putString("location", place[0] + ", " + place[1]);
-            editor.putString("zipCode", place[2]);
-            editor.commit();
-            String city = sharedPreferences.getString("location","");
+
+        city = sharedPreferences.getString("location", "");
+        location = mContentView.findViewById(R.id.location_button);
             location.setText(city.split(",")[0]);
+        getNearByLocation();
 
 
-        }
+    }
+
+
+    public ProgressDialog showProgressDialog(FragmentActivity activity, String message) {
+        ProgressDialog m_Dialog = new ProgressDialog(activity);
+        m_Dialog.setMessage(message);
+        m_Dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        m_Dialog.setCancelable(false);
+        m_Dialog.show();
+        return m_Dialog;
+
+    }
+
+    public void getNearByLocation() {
+
+        //  ProgressDialog progressDialog = showProgressDialog(getActivity(),"loading Near By Locations");
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String zipCode = sharedPreferences.getString("zipCode", "");
+        nearByZipCodeList = new ArrayList<>();
+        nearByZipCodeList.add(Integer.parseInt(zipCode.trim()));
+        String distance = "30";
+        StringBuilder url = new StringBuilder();
+        url.append("https://www.zipcodeapi.com/rest/j8e1e5TnyhPXPweFQMWcKRExQOxtdAoxviq6JCJeYfiunhnKW965UBd4iAE8vRzj/radius.json/");
+        url.append(zipCode.trim() + "/");
+        url.append(distance + "/mile");
+        // String urll = " https://www.zipcodeapi.com/rest/j8e1e5TnyhPXPweFQMWcKRExQOxtdAoxviq6JCJeYfiunhnKW965UBd4iAE8vRzj/radius.json/64468/100/mile";
+
+        new GetNearByLocation(url.toString(), this, context).execute();
 
     }
 
     @Override
-    public void setUserVisibleHint(boolean visible) {
-        super.setUserVisibleHint(visible);
-        if (visible && isResumed()) {
-            onStart();
+    public void onPostExecute(JSONObject jsonResponse) {
+
+        if (jsonResponse != null) {
+            JSONArray zipCodes = null;
+            try {
+
+                if (!jsonResponse.isNull("isNotOnline") && jsonResponse.getBoolean("isNotOnline")) {
+                    Toast.makeText(context, "you are not online", Toast.LENGTH_LONG).show();
+                } else {
+                    zipCodes = jsonResponse.getJSONArray("zip_codes");
+
+                    for (int i = 0; i < zipCodes.length(); i++) {
+                        JSONObject zipCode = zipCodes.getJSONObject(i);
+                        nearByZipCodeList.add(Integer.parseInt(zipCode.getString("zip_code").trim()));
+                    }
+                    findObjects();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
-
     private void findObjects() {
-
-
         final GridView listView = mContentView.findViewById(R.id.item_gridview);
         final App globalVariable = (App) context.getApplicationContext();
         // Configure Query
         ParseQuery<ParseObject> query = new ParseQuery<>("items");
 
-        ParseUser currentUser = ParseUser.getCurrentUser();
-        String email = currentUser.getEmail();
+        String currentUser = ParseUser.getCurrentUser().getUsername();
+        //String email = currentUser.getEmail();
+        query.whereNotEqualTo("username", currentUser);
         // Query Parameters
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-        String zipCode = sharedPreferences.getString("zipCode","");
-
-        query.whereEqualTo("zipCode", Integer.parseInt(zipCode.trim()));
+        query.whereContainedIn("zipCode", nearByZipCodeList);
+        if (searchText != null && !searchText.isEmpty()) {
+            query.whereFullText("title", searchText);
+        }
+        // query.whereEqualTo("zipCode", Integer.parseInt(zipCode.trim()));
 
         // Sorts the results in ascending order by the itemName field
         //  query.orderByAscending("itemName");
         String c = query.getClassName();
-       // final ProgressDialog progressDialog = showProgressDialog(getActivity(),"loading");
+        final ProgressDialog progressDialog = showProgressDialog(getActivity(), "loading data");
         query.findInBackground(new FindCallback<ParseObject>() {
             Bitmap bitmap;
 
             @Override
             public void done(List<ParseObject> objects, final ParseException e) {
-           //     progressDialog.dismiss();
+                progressDialog.dismiss();
                 if (e == null) {
-
                     // Adding objects into the Array
                     for (int i = 0; i < objects.size(); i++) {
                         ParseFile file = objects.get(i).getParseFile("download");
                         final String title = objects.get(i).getString("title");
-                        //   ParseFile file = (ParseFile) objects.get(i).get("download");
-
                         final Date postDate = objects.get(i).getCreatedAt();
+                        final String objectId = objects.get(i).getObjectId();
+                        final String price = Double.toString(objects.get(i).getDouble("price"));
+
                         if (file != null) {
                             file.getDataInBackground(new GetDataCallback() {
                                 @Override
@@ -202,61 +272,27 @@ public class HomeFragment extends Fragment {
                                                 .decodeByteArray(
                                                         data, 0,
                                                         data.length);
-
-                                        dataList.add(new ItemModel(title, postDate, bitmap));
-                                        ItemAdapter itemAdapter = new ItemAdapter(context, R.layout.gridview_items, dataList);
+                                        ItemModel itemModel = new ItemModel(title, postDate, ComUtils.getResizedBitmap(bitmap, 250,
+                                                250), objectId, price);
+                                        dataList.add(itemModel);
+                                        itemGridview.setAdapter(itemAdapter);
                                         itemAdapter.notifyDataSetChanged();
-
                                     }
 
                                 }
                             });
                         }
-
                     }
+
+
                 } else {
 
                 }
 
             }
         });
-    }
-
-    public static ProgressDialog showProgressDialog(Activity activity, String message) {
-        ProgressDialog m_Dialog = new ProgressDialog(activity);
-        m_Dialog.setMessage(message);
-        m_Dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        m_Dialog.setCancelable(false);
-        m_Dialog.show();
-        return m_Dialog;
 
     }
 
-    public void getNearByLocation(){
-        String googleAPIKey = getResources().getString(R.string.google_maps_key);
-        RequestQueue requestQueue = Volley.newRequestQueue(context);
-      //  String requesturl="https://maps.googleapis.com/maps/api/place/search/json?radius=500&sensor=false&key="+googleAPIKey+"&location=13.01,74.79";
-        String requesturl = "http://pastebin.com/raw/2bW31yqa";
-        // final ProgressDialog progressDialog = showProgressDialog(getActivity(),"nearby");
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, requesturl, null, new Response.Listener<JSONObject>() {
 
-                    @Override
-                    public void onResponse(JSONObject response) {
-                       String res = response.toString();
-
-                    }
-                }, new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // TODO: Handle error
-
-
-                    }
-                });
-        requestQueue.add(jsonObjectRequest);
-
-
-}
 }
