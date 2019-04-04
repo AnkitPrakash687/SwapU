c41138eb7a18ce317c5b03609d7681383d959790
Ankit Prakash <43018227+AnkitPrakash687@users.noreply.github.com>
2019-04-04 00:01:58 -0500

    S533987 - added internet connection check on login

--- app/src/main/java/com/example/swapu/login/LoginActivity.java
+++ app/src/main/java/com/example/swapu/login/LoginActivity.java
@@ -3,17 +3,18 @@ package com.example.swapu.login;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.swapu.App;
-import com.example.swapu.home.HomeActivity;
 import com.example.swapu.R;
+import com.example.swapu.common.AppStatus;
+import com.example.swapu.home.HomeActivity;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
@@ -40,42 +41,49 @@ public class LoginActivity extends AppCompatActivity {
 
     public void login(View view) {
         /*added validation to check if email is blank*/
-        if( TextUtils.isEmpty(edEmail.getText())){
-            edEmail.setError( "Email is required!" );  
-        /*added validation to check if password is blank*/
-        }else if( TextUtils.isEmpty(edPassword.getText())){
-            edPassword.setError( "Password is required!" );
-        }else{
-            final ProgressDialog progress = new ProgressDialog(this);
-            progress.setMessage("Loading ...");
-            progress.show();
-            ParseUser.logInInBackground(edEmail.getText().toString(), edPassword.getText().toString() , new LogInCallback() {
-                @Override
-                public void done(ParseUser parseUser, ParseException e) {
-                    progress.dismiss();
-                    if (parseUser != null) {
-                        String location = ParseUser.getCurrentUser().get("location").toString();
-                        String zipcode = ParseUser.getCurrentUser().get("zipCode").toString();
-                        final App globalVariable = (App) getApplicationContext();
+
+        if (!AppStatus.getInstance(this).isOnline()) {
+
+            Toast.makeText(this, "You are not online!!!!", Toast.LENGTH_SHORT).show();
+
+        } else {
+            if (TextUtils.isEmpty(edEmail.getText())) {
+                edEmail.setError("Email is required!");
+                /*added validation to check if password is blank*/
+            } else if (TextUtils.isEmpty(edPassword.getText())) {
+                edPassword.setError("Password is required!");
+            } else {
+                final ProgressDialog progress = new ProgressDialog(this);
+                progress.setMessage("Loading ...");
+                progress.show();
+                ParseUser.logInInBackground(edEmail.getText().toString(), edPassword.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser parseUser, ParseException e) {
+                        progress.dismiss();
+                        if (parseUser != null) {
+                            String location = ParseUser.getCurrentUser().get("location").toString();
+                            String zipcode = ParseUser.getCurrentUser().get("zipCode").toString();
+                            final App globalVariable = (App) getApplicationContext();
 //                        globalVariable.setLocation(location);
 //                        globalVariable.setPincode(zipcode);
-                        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                        SharedPreferences.Editor editor = sharedPreferences.edit();
-                        editor.putString("location", location);
-                        editor.putString("zipCode", zipcode);
-                        editor.commit();
-                        Toast.makeText(LoginActivity.this, "Welcome back!", Toast.LENGTH_LONG).show();
-                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                        startActivity(intent);
-                        finish();
-                    } else {
-                        ParseUser.logOut();
-                        Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putString("location", location);
+                            editor.putString("zipCode", zipcode);
+                            editor.commit();
+                            Toast.makeText(LoginActivity.this, "Welcome back!", Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            ParseUser.logOut();
+                            Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
                     }
-                }
-            });
-        }
+                });
+            }
 
+        }
     }
 
     public void signup(View view) {
